2475a78e24b23beae1805350ed7a33b6baf71ea5
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-07-07 11:18:59 +0600

    added methods medicineService

--- src/service/MedicineService.java
+++ src/service/MedicineService.java
@@ -1,4 +1,12 @@
 package service;
 
+import models.Medicine;
+
+import java.util.List;
+
 public interface MedicineService {
+    List<Medicine>getallMedicineByPharmacy(String pharmacyName);
+    void addMedicineToPharmacy(Long pharmacyId, Medicine medicine);
+    void updateMedicinePrice(Long pharmacyId,Long medicineId,int price);
+    void  deleteMedicineByName(Long pharmacyId,String medicineName);
 }
